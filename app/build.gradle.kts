2ce9b99d34d4f505d862bb412220161a42f14ae8
ibarguipas <ibarguipas@educacion.navarra.es>
2024-01-24 21:00:18 +0100

    Falta el random

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.jugando_con_botones"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.jugando_con_botones"
--- app/src/main/java/com/example/jugando_con_botones/MainActivity.kt
+++ app/src/main/java/com/example/jugando_con_botones/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.jugando_con_botones
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private val numBotones = 10
+    private lateinit var llBotonera: LinearLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        llBotonera = findViewById(R.id.llBotonera)
+
+        val lp = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT, resources.getDimensionPixelSize(R.dimen.button_height)
+        )
+        for (i in 0 until numBotones) {
+            val button = Button(this)
+            button.layoutParams = lp
+            button.text = "Botón " + String.format("%02d", i)
+            button.setOnClickListener(buttonClickListener(i))
+            llBotonera.addView(button)
+        }
     }
+
+    private fun buttonClickListener(index: Int): View.OnClickListener {
+
+        return  View.OnClickListener {
+            Toast.makeText(this@MainActivity,
+                "Botón " + String.format("%02d", index) + " pulsado.", Toast.LENGTH_SHORT).show()
+
+        }
+
+    }
+
+
 }
